37f5c76b30dad0a412da8b64eb1cac7c195e8805
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-17 13:58:39 -0300

    importar arquivos de rotas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import express from 'express';
 
+import routes from './routes/index';
+
 const app = express();
 
 const port = '8080';
@@ -8,5 +10,7 @@ const port = '8080';
 mongoose.connect('mongodb://localhost:27017/vizir', { useCreateIndex: true, useNewUrlParser: true })
   .then(() => process.stdout.write('MongoDB connected!\n'))
   .catch(err => process.stdout.write(err));
+
+app.use('/api', routes);
 
 app.listen(port, () => process.stdout.write(`Listening on port ${port}\n`));
