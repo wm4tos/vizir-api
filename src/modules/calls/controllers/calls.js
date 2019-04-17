5a70d26f779c6906a855f274640b40dd9c6f295c
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-17 17:15:57 -0300

    criar método que lista todas as possíveis ligações

--- src/modules/calls/controllers/calls.js
+++ src/modules/calls/controllers/calls.js
@@ -1,4 +1,13 @@
+import Calls from '../models/calls';
+
 class CallsController {
+  static All(filter = {}) {
+    try {
+      return Calls.find(filter);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 }
 
 export default CallsController;
