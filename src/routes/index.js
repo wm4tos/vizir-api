f0a6a3d7646d8bab7a625efd005bbd86cf208d6c
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-17 18:00:04 -0300

    método que lista planos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import CallsController from '../modules/calls/controllers/calls';
+import PlansController from '../modules/plans/controllers/plans';
 import { Emitter } from '../services/emitter';
 
 const router = express.Router();
@@ -26,6 +27,32 @@ router.route('/calls')
         response = {
           status: 404,
           data: calls,
+          message: 'Nenhum registro encontrado :/',
+        };
+      }
+    } catch (err) {
+      response = {
+        status: 500,
+        message: err,
+      };
+    }
+    return Emitter(res, response);
+  });
+
+router.route('/plans')
+  .get(async (req, res) => {
+    let response;
+    try {
+      const plans = await PlansController.Get(req.query);
+      if (plans.length) {
+        response = {
+          status: 200,
+          data: plans,
+        };
+      } else {
+        response = {
+          status: 404,
+          data: plans,
           message: 'Nenhum registro encontrado :/',
         };
       }
