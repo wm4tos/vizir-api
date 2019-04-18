1bcd03bd278b332d11f83afd7bb532bfc874f6b8
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-18 17:45:57 -0300

    criar função para cadastrar nova ligação

--- src/modules/calls/controllers/calls.js
+++ src/modules/calls/controllers/calls.js
@@ -1,7 +1,12 @@
 import Calls from '../models/calls';
-import { Emitter } from '../../../services/emitter';
+import { Emitter, ErrorEmitter } from '../../../services/emitter';
 
 class CallsController {
+  /**
+   * @description Valida se a requisição enviada pelo usuário é válida.
+   * @param {Object} req
+   * @param {Object} res
+   */
   static async Get(req, res) {
     Object.keys(req.query).forEach(x => (req.query[x] ? null : delete req.query[x]));
     let response;
@@ -13,20 +18,53 @@ class CallsController {
           data: calls,
         };
       } else {
-        res.status(404);
-        response = {
-          status: 404,
-          message: 'Nenhum registro encontrado :/',
-        };
+        return ErrorEmitter(res, 404);
       }
     } catch (err) {
-      res.status(500);
-      response = {
-        status: 500,
-        message: err,
-      };
+      return ErrorEmitter(res, 500, err);
     }
     return Emitter(res, response);
+  }
+
+  /**
+   * @description Valida se a requisição enviada pelo usuário é válida.
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   */
+  static async ValidateQuery(req, res, next) {
+    if (Object.keys(req.body).length !== 2) {
+      return ErrorEmitter(res, 400);
+    }
+    Object.keys(req.body).forEach((x) => {
+      if (!req.body[x]
+        && !((x === 'origin' && typeof x === 'object')
+        || !(x === 'destinys' && !x.length && typeof x === 'object'))) {
+        return ErrorEmitter(res, 400);
+      }
+    });
+    const { origin } = req.body;
+    const call = await Calls.find({ origin });
+    if (call.length) {
+      return ErrorEmitter(res, 409, 'Essa origem já tá cadastrada no banco. Pode tentar outra?');
+    }
+    return next();
+  }
+
+  /**
+   * @description Valida se a requisição enviada pelo usuário é válida.
+   * @param {Object} req
+   * @param {Object} res
+   */
+  static async Create(req, res) {
+    const { origin, destinys } = req.body;
+    try {
+      const newCall = new Calls({ origin, destinys });
+      await newCall.save();
+      return Emitter(res, { status: 200, data: newCall, message: 'Usuário cadastrado com sucesso!' });
+    } catch (err) {
+      return ErrorEmitter(res, 500, err);
+    }
   }
 }
 
