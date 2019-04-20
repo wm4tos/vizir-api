bea72181dc65d1bec533b66799b1d6498c100996
Wesley Matos <wrickee@gmail.com>
2019-04-20 12:43:25 -0300

    incluir função que busca apenas um plano

--- src/modules/plans/services/plans.js
+++ src/modules/plans/services/plans.js
@@ -1,3 +1,5 @@
 import Plans from '../models/plans';
 
 export const GetPlans = async (filter = {}) => await Plans.find(filter);
+
+export const GetPlan = async (filter = {}) => await Plans.findOne(filter)
