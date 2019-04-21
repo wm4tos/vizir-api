be71434f5db4ff13445dd9f3e746bf08020a4aa4
Wesley Matos <wrickee@gmail.com>
2019-04-21 19:31:18 -0300

    CORS permitindo todos os métodos

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL, { useCreateIndex: true, useNewUrlParser:
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 app.use(bodyParser.json());
