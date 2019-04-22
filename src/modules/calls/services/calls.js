89d266ecf67b7e2c10da5b6804e7eafa7826d980
Wesley Matos <wrickee@gmail.com>
2019-04-22 00:34:01 -0300

    exibir erro se houver um destino vazio

--- src/modules/calls/services/calls.js
+++ src/modules/calls/services/calls.js
@@ -18,6 +18,7 @@ export const GetCalls = async (filter = {}) => new Promise(async (resolve, rejec
 
 export const NewCall = call => new Promise(async (resolve, reject) => {
   if (!('origin' in call) || !('destinys' in call)) return reject('Parâmetros inválidos.');
+  if (call.destinys.find(x => !x.destiny)) return reject('Ei... Falta alguma coisa, né?');
   call.destinys.forEach(x => x.id = Random.id(24));
   try {
     const newCall = new Calls(call);
@@ -41,6 +42,7 @@ export const UpdateCall = async callToUpdate => new Promise(async (resolve, reje
   if (!('origin' in callToUpdate)
   || !('destinys' in callToUpdate)
   || !('_id' in callToUpdate)) return reject('Parâmetros inválidos.');
+  if (callToUpdate.destinys.find(x => !x.destiny)) return reject('Ei... Falta alguma coisa, né?');
   const { _id } = callToUpdate;
   callToUpdate.destinys.forEach(x => (x.id ? null : x.id = Random.id(24)));
   try {
